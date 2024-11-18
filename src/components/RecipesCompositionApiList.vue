58b07fac4b66c04e3316cc8b036e3bc6cd939215
supervyy <hallo>
2024-11-18 02:20:11 +0100

    initial

---
+++ src/components/RecipesCompositionApiList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+defineProps<{ title: string }>()
+const recipes = ref([])
+
+</script>
+
+
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
